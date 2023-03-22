db96268ccf2f0d915a0174d5ca56a3318fbe7ea8
Trupti satsangi <126486792+truptiQL@users.noreply.github.com>
2023-03-22 15:52:09 +0530

    Added necessary comments

--- Promise.js
+++ Promise.js
@@ -1,9 +1,9 @@
 class _Promise {
   constructor(func) {
-    this.state = "pending";
+    this.state = "pending";       //there are 3 states that can be "pending", "resolved", or "rejected"
     this.data = undefined;
 
-    func(this._resolve.bind(this), this._reject.bind(this));
+    func(this._resolve.bind(this), this._reject.bind(this));    //binding argument of function passed in constructor with _resolve and _reject method
   }
 
   _resolve(value) {
@@ -16,7 +16,7 @@ class _Promise {
     this.data = value;
   }
 
-  then(onFulfilled, onReject = () => {}) {
+  then(onFulfilled, onReject = () => {}) {          //Error can also be handled in then also, then is returning promise
     return new _Promise((resolve, reject) => {
       if (this.state === "resolved") {
         try {
@@ -32,7 +32,7 @@ class _Promise {
       } else if (this.state === "rejected") {
         try {
           const result = onReject(this.data);
-          if (result instanceof _Promise) {
+          if (result instanceof _Promise) {         
             result.then(resolve, reject);
           } else {
             resolve(result);
@@ -45,7 +45,7 @@ class _Promise {
   }
 
   catch(onReject) {
-    return new _Promise((resolve, reject) => {
+    return new _Promise((resolve, reject) => {      // catch also returns promise
       if (this.state === "rejected") {
         try {
           const result = onReject(this.data);
